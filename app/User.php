2c0d0d17b04ccdc14adabc5444785d9461781902
semihdumannn <semihdumannn@gmail.com>
2018-03-29 19:27:12 +0300

    Proje oluştu

---
+++ app/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name','surname', 'email', 'password','rutbe','son_giris','avatar'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function yonetici_mi()
+    {
+        if ($this->rutbe == 1)
+            return true;
+        else
+            return false;
+    }
+}
